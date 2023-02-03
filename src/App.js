95bfea59abad7f1f3e93c1111dff29628f8d3e26
Daria Fedorova <dasha-march@ya.ru>
2023-02-03 08:54:15 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+
+const API_KEY = '081891c3558231a7772c677f6123db3b'
 
 function App() {
+  const [weather, setWeather] = useState({})
+  const [city, setCity] = useState('London')
+  const [cityInput, setCityInput] = useState('')
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${API_KEY}`,
+        {
+          referrerPolicy: 'no-referrer-when-downgrade',
+        }
+      )
+      const data = await response.json()
+      setWeather(data)
+    }
+    fetchData()
+  }, [city])
+
+  function onSubmit(e) {
+    e.preventDefault()
+    setCity(cityInput)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Салам алейкум</h1>
       </header>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a city"
+          value={cityInput}
+          onChange={(e) => setCityInput(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <article>
+        {weather.main ? (
+          <div className="weather-card">
+            <p className="temperature">
+              Температура: {(weather.main.temp - 273.15).toFixed(1)}
+            </p>
+            <p>Влажность: {weather.main.humidity}</p>
+            <p className="description">
+              Описание: {weather.weather[0].description}
+            </p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </article>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
